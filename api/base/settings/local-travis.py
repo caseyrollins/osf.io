bd7921b21a9d343404e192e644185c034420fd12
Casey Rollins <case.rollins@gmail.com>
2017-07-12 11:49:12 -0400

    WIP

--- api/base/settings/local-travis.py
+++ api/base/settings/local-travis.py
@@ -1,6 +1,19 @@
 from .defaults import *  # noqa
 
 
+# DATABASES = {
+#     'default': {
+#         'CONN_MAX_AGE': 0,
+#         'ENGINE': 'osf.db.backends.postgresql',  # django.db.backends.postgresql
+#         'NAME': os.environ.get('OSF_DB_NAME', 'osf'),
+#         'USER': os.environ.get('OSF_DB_USER', 'postgres'),
+#         'PASSWORD': os.environ.get('OSF_DB_PASSWORD', ''),
+#         'HOST': os.environ.get('OSF_DB_HOST', '127.0.0.1'),
+#         'PORT': os.environ.get('OSF_DB_PORT', '54321'),
+#         'ATOMIC_REQUESTS': True,
+#     }
+# }
+
 VARNISH_SERVERS = ['http://127.0.0.1:8080']
 ENABLE_VARNISH = True
 ENABLE_ESI = False
